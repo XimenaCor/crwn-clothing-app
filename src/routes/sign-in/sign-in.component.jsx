a26fd67b71ff6be593015521f3243e1c21914ff8
Ximena Cordero <cor.xime@gmail.com>
2022-05-13 13:55:43 -0400

    still working with firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,26 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+/* import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth"; */
+
+import {
+  // auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  // signInWithGoogleRedirect
+} from "../../utils/firebase/firebase.utils";
+/* import { async } from "@firebase/util"; */
 
 const SignIn = () => {
+
+/*   useEffect(() => {
+    async function fetchData () {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    }
+    fetchData();
+  }, []); */
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -10,6 +30,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+      {/* <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button> */}
     </div>
   )
 }
