8d44420ed5d78529de91014c3fe594b574ac04a8
Ximena Cordero <cor.xime@gmail.com>
2022-09-04 21:58:53 -0400

    add observer for auth state changes

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,16 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 import './navigation.styles.scss'
 
 
 //A frangment is a react component that actually renders to nothing when it gets mounted in the dom, si it's not an specific html element
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
@@ -17,11 +21,18 @@ const Navigation = () => {
           <Link className="nav-link" to='/shop'>
             SHOP
           </Link>
-        </div>
-        <div className="nav-links-container">
-          <Link className="nav-link" to='/auth'>
-            SIGN IN
-          </Link>
+          {
+            currentUser ? (
+              <span className="nav-link" onClick={signOutUser}>
+                {' '}
+                SIGN OUT{' '}
+              </span>
+            ) : (
+              <Link className="nav-link" to='/auth'>
+                SIGN IN
+              </Link>
+            )
+          }
         </div>
       </div>
       <Outlet />
