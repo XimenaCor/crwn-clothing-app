dfeda9120685e6cf3b429b247d65ecdb39356677
Ximena Cordero <cor.xime@gmail.com>
2022-09-07 19:57:18 -0400

    adding cart context and shopping icon as well as cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,13 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import './navigation.styles.scss'
 
@@ -10,6 +15,7 @@ import './navigation.styles.scss'
 //A frangment is a react component that actually renders to nothing when it gets mounted in the dom, si it's not an specific html element
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -33,7 +39,12 @@ const Navigation = () => {
               </Link>
             )
           }
+          <CartIcon />
         </div>
+        {/* what is going to returned here is the last thing you gave, which is the component */}
+        {
+          isCartOpen && <CartDropdown />
+        }
       </div>
       <Outlet />
     </Fragment>
